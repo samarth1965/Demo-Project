d2fb05e25568e21fedc677b1e7e23e93ce24f952
Srivastava <samarth.srivastava@capgemini.com>
2024-07-31 14:36:31 +0530

    dashboard changes

--- Front-end Demo Project/food-delivery/src/app/pages/dashboard/dashboard.component.ts
+++ Front-end Demo Project/food-delivery/src/app/pages/dashboard/dashboard.component.ts
@@ -3,24 +3,27 @@ import { RouterModule } from '@angular/router';
 import { FoodResponse } from '../../model/food-response';
 import { FoodServiceService } from '../../services/food-service.service';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterModule,CommonModule,ReactiveFormsModule],
+  imports: [RouterModule,CommonModule,ReactiveFormsModule,FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit{
 
   foods: FoodResponse[] = [];
+  searchInput: string='';
 
   constructor(private foodService: FoodServiceService){}
   ngOnInit(): void {
     throw new Error('Method not implementedssss.');
   }
 
-  // searchByFoodName()
+  searchByFoodName(){
+
+  }
 
 }
